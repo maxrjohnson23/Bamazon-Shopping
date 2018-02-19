01c6970b76ca0d4bcd46ded9199f6f7da8a628fd
Max Johnson <maxrjohnson23@gmail.com>
2018-02-19 13:21:27 -0600

    Update sample product data

--- sql/seed.sql
+++ sql/seed.sql
@@ -1,3 +1,4 @@
+# DML for seeding sample product data
 USE BAMAZON;
 
 INSERT into PRODUCTS (product_name, department_name, price, stock_quantity) values ('Samsung Galaxy Note 8 - 64GB', 'Electronics', '800.00','5');
@@ -8,9 +9,6 @@ INSERT into PRODUCTS (product_name, department_name, price, stock_quantity) valu
 INSERT into PRODUCTS (product_name, department_name, price, stock_quantity) values ('NutriBullet Blender', 'Small Appliances', '110.00','5');
 INSERT into PRODUCTS (product_name, department_name, price, stock_quantity) values ('Dinner Plates - White', 'Home', '12.00','20');
 INSERT into PRODUCTS (product_name, department_name, price, stock_quantity) values ('Copper Mugs', 'Home', '15.00','10');
-INSERT into PRODUCTS (product_name, department_name, price, stock_quantity) values ('Watch', 'Clothing', '25.00','10');
+INSERT into PRODUCTS (product_name, department_name, price, stock_quantity) values ('Timex Watch', 'Clothing', '25.00','10');
 INSERT into PRODUCTS (product_name, department_name, price, stock_quantity) values ('Jeans', 'Clothing', '45.00','35');
-INSERT into PRODUCTS (product_name, department_name, price, stock_quantity) values ('T-shirts', 'Clothing', '10.00','10');
-
-
-select * from PRODUCTS;
+INSERT into PRODUCTS (product_name, department_name, price, stock_quantity) values ('T-shirts (Black)', 'Clothing', '10.00','10');
